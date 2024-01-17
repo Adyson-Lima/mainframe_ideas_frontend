82ea4a5b8d0c61a6941b39a3f5c6e7b5b83ec372
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-17 08:23:30 -0300

    10° commit - tdd tela Ideas: Existe botao excluir em Ideas? RED

--- src/tests/Ideas.test.js
+++ src/tests/Ideas.test.js
@@ -28,4 +28,8 @@ describe('Testes da tela Ideas', () => {
     expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
   });
 
+  it('Existe botao excluir em Ideas?', () => {
+    expect(screen.getByTestId('mybtn2')).toBeInTheDocument();
+  });
+
 });
