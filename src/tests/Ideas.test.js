9a8cebfaa53e0f413f41c361c4306597cb36f7b4
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-17 08:02:51 -0300

    2° commit - tdd tela Ideas: Existe card em Ideas? RED

---
+++ src/tests/Ideas.test.js
@@ -0,0 +1,19 @@
+import {render, screen} from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Ideas from '../pages/Ideas';
+
+describe('Testes da tela Ideas', () => {
+
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Ideas/>
+      </BrowserRouter>
+    );
+  });
+
+  it('Existe card em Ideas', () => {
+    expect(screen.getByTestId('mycard')).toBeInTheDocument();
+  });
+
+});
