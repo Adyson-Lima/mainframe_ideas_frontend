dac9fe06b99caa662a674225952a3b55abc7fd68
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-17 09:05:23 -0300

    26° commit - logica delete implementada

--- src/pages/Ideas/index.js
+++ src/pages/Ideas/index.js
@@ -22,6 +22,16 @@ export default function Ideas(){
     }
   }
 
+  // delete, remove um registro na api
+  async function deleteIdea(id){
+    try {
+      await api.delete(`api/v1/ideas/${id}`,{});
+      setIdeas(my_ideas.filter(idea => idea.id !== id));
+    } catch (error) {
+      alert('Erro ao excluir');
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -55,7 +65,8 @@ export default function Ideas(){
                     onClick={() => updateIdea(idea.id)}>Editar</button>
 
                     <button data-testid="mybtn2" type="button"
-                    className="btn btn-outline-danger">Excluir</button>
+                    className="btn btn-outline-danger" style={{margin: '2px'}}
+                    onClick={() => deleteIdea(idea.id)}>Excluir</button>
 
                   </td>
               </tr>
