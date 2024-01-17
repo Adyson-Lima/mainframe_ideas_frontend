82ef0682b2c894c1bcd8b65f94299aefa9257036
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-17 08:35:01 -0300

    14° commit - tdd tela NewUpdate: Existe link Home em Newupdate? RED

--- src/tests/NewUpdate.test.js
+++ src/tests/NewUpdate.test.js
@@ -16,4 +16,8 @@ describe('Testes da tela NewUpdate', () => {
     expect(screen.getByTestId('mycard')).toBeInTheDocument();
   });
 
+  it('Existe link Home em Newupdate?', () => {
+    expect(screen.getByTestId('mylink')).toBeInTheDocument();
+  });
+
 });
